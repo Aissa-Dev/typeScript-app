39f7787e18f2386e3a3774b63b3753ffd95ee58f
aissamihi <aissamihi@gmail.com>
2021-09-26 00:52:47 -0400

    public, private, readonly, Constructor

--- public/app.js
+++ public/app.js
@@ -1,8 +1,29 @@
 "use strict";
-var anchor = document.querySelector("a"); // ! : means it explicityly exist
-//console.log(anchor.href);
-var form1 = document.querySelector("form");
-//console.log(form1?.children); // because we didnt add !
+//Classes
+var Invoice = /** @class */ (function () {
+    //   public client: string;
+    //   private amount: number;
+    //   readonly details: string;
+    //   constructor(c: string, a: number, d: string) {
+    //     this.client = c;
+    //     this.amount = a;
+    //     this.details = d;
+    //   }
+    function Invoice(client, amount, details) {
+        var _this = this;
+        this.client = client;
+        this.amount = amount;
+        this.details = details;
+        this.format = function () {
+            return _this.client + " owes $" + _this.amount + " for " + _this.details;
+        };
+    }
+    return Invoice;
+}());
+console.log("testing");
+var invOne = new Invoice("aissa", 200, "web");
+var invTwo = new Invoice("rachid", 300, "mobile");
+console.log(invOne, invTwo);
 var form2 = document.querySelector(".new-item-form"); // ts dont know the type of element only by className
 //console.log(form2.children);
 var type = document.querySelector("#type");
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
-const anchor = document.querySelector("a")!; // ! : means it explicityly exist
-//console.log(anchor.href);
+//Classes
+class Invoice {
+  //   public client: string;
+  //   private amount: number;
+  //   readonly details: string;
 
-const form1 = document.querySelector("form");
-//console.log(form1?.children); // because we didnt add !
+  //   constructor(c: string, a: number, d: string) {
+  //     this.client = c;
+  //     this.amount = a;
+  //     this.details = d;
+  //   }
+
+  constructor(
+    public client: string,
+    private amount: number,
+    readonly details: string
+  ) {}
+
+  format = () => {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  };
+}
+console.log("testing");
+
+const invOne = new Invoice("aissa", 200, "web");
+const invTwo = new Invoice("rachid", 300, "mobile");
+console.log(invOne, invTwo);
 
 const form2 = document.querySelector(".new-item-form") as HTMLFormElement; // ts dont know the type of element only by className
 //console.log(form2.children);
